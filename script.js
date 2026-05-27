fc16cda73b6aa720a880b9b58d4c5a00f962d8c3
shreedeepak-s <240110.ad@rmkec.ac.in>
2026-05-27 12:00:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,57 @@
-function navigateTo(screen) {
-  // Hide all screens
-  document.querySelectorAll('.screen').forEach(s => s.classList.add('hidden'));
+let responses = [];
 
-  // Show selected screen
-  document.getElementById(`${screen}-screen`).classList.remove('hidden');
+function submitSurvey() {
+
+    let username = document.getElementById("username").value;
+    let task = Number(document.getElementById("task").value);
+    let ease = Number(document.getElementById("ease").value);
+    let satisfaction = Number(document.getElementById("satisfaction").value);
+    let feedback = document.getElementById("feedback").value;
+
+    let userData = {
+        username,
+        task,
+        ease,
+        satisfaction,
+        feedback
+    };
+
+    responses.push(userData);
+
+    generateReport();
+}
+
+function generateReport() {
+
+    let totalTask = 0;
+    let totalEase = 0;
+    let totalSatisfaction = 0;
+
+    responses.forEach(user => {
+        totalTask += user.task;
+        totalEase += user.ease;
+        totalSatisfaction += user.satisfaction;
+    });
+
+    let avgTask = (totalTask / responses.length).toFixed(2);
+    let avgEase = (totalEase / responses.length).toFixed(2);
+    let avgSatisfaction = (totalSatisfaction / responses.length).toFixed(2);
+
+    let report = `
+        <h3>Usability Analysis Report</h3>
+        <p><b>Total Participants:</b> ${responses.length}</p>
+        <p><b>Average Task Completion:</b> ${avgTask}</p>
+        <p><b>Average Ease of Use:</b> ${avgEase}</p>
+        <p><b>Average Satisfaction:</b> ${avgSatisfaction}</p>
+    `;
+
+    responses.forEach(user => {
+        report += `
+            <hr>
+            <p><b>User:</b> ${user.username}</p>
+            <p><b>Feedback:</b> ${user.feedback}</p>
+        `;
+    });
+
+    document.getElementById("results").innerHTML = report;
 }
